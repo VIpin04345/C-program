2167bb7e6f92233df5cf20c184df636a28c1a152
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-25 15:26:43 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -11,3 +11,13 @@ int main(){
 void main(){
     printf("Welcom to programming world");
 }
+
+
+#include<stdint.h>
+#include<conio.h>
+void main(){
+    int a = 12;
+    int b = 13;
+    printf("%d",a);
+    printf("%d", b);
+}
