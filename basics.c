5bedf80b02b2d81124c6c440dd200a7682fe5e18
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-26 13:56:42 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -165,3 +165,23 @@ int main(){
     return 0;
 }
 
+
+# include<stdio.h>
+int main(){
+    int marks;
+    printf("enter your marks:-");
+    scanf("%d", &marks);
+    if (marks<30){
+        printf("grade c \n");
+    }
+    else if (marks>=30 && marks<70){
+        printf("grade b \n");
+    }
+    else if (marks>=70 && marks<90){
+        printf("grade a \n");
+    }
+    else{
+    printf("grade a++");
+    }
+}
+
