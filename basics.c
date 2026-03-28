9fadc188fdf7f285f7394f20a95bd35b59f30288
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-28 15:08:34 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -413,3 +413,13 @@ int main(){
     }
    return 0
 }
+
+#include<stdio.h>
+int main(){
+    for (int i = 1; i <= 5;i++){
+        if(i==3){
+            break;
+        }
+        printf("%d \n", i);
+    }
+}
