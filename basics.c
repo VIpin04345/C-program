19ec0f7ecbfe523f5f3bdefb557059744571a31f
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-28 15:09:50 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -444,3 +444,15 @@ int main(){
         printf("%c \n", s);
     }
 }
+
+
+#include<stdio.h>
+int main(){
+    int  i = 1;
+    while (i<=5)
+    {
+        printf("%d \n", i);
+        i++;
+    }
+    
+}
