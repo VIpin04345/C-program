a2b62769709c9dad15fe4d15fc4da7324817c95d
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-31 13:30:49 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -677,15 +677,15 @@ int main(){
 }
 
 
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     printf("enter a number:-");
-//     scanf("%d", &n);
-//     for (int i = 1; i <= n;i++){
-//         printf("%d\n", i);
-//     }
-// }
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number:-");
+    scanf("%d", &n);
+    for (int i = 1; i <= n;i++){
+        printf("%d\n", i);
+    }
+}
 
 
 // #include<stdio.h>
