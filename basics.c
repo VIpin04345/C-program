2c16858aee624a4d8dc4710db63846ce3ce2ebc9
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-31 13:32:29 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -779,19 +779,19 @@ int main(){
 
 // # write a program to print only even number between 1 to n.
 
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     printf("enter a number:-");
-//     scanf("%d", &n);
-//     int i = 1;
-//     while(i<=n){
-//         if(i%2==0){
-//             printf("%d\n", i);
-//         }
-//         i++;
-//     }
-// }
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number:-");
+    scanf("%d", &n);
+    int i = 1;
+    while(i<=n){
+        if(i%2==0){
+            printf("%d\n", i);
+        }
+        i++;
+    }
+}
 
 
 
