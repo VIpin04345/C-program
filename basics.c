7d544a36b788f8af819c14dbaf3b1a6fe1f73aa1
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-05 20:15:45 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -377,6 +377,20 @@ int main(){
     }
 }
 
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int year;
+    printf("enter your yaer:-");
+    scanf("%d", &year);
+    if (year%4==0 && year%50!=0 || year%400==0){
+        printf("%d is a leap year \n",year);
+    }
+    else{
+        printf("%d is not a leap year \n", year);
+    }
+}
+
 
 #include<stdio.h>
 #include<conio.h>
