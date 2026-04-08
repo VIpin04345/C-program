9f6c742872626487f3a3dd1caff9f3395c0e80c3
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-08 15:41:09 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1578,14 +1578,14 @@ int main(){
 
 
 
-// #include<stdio.h>
-// #include<conio.h>
-// void printNumbers(int arr[], int n);
+#include<stdio.h>
+#include<conio.h>
+void printNumbers(int arr[], int n);
 
-// void main(){
-//     int arr[] = {1, 2, 3, 4, 5, 6};
-//     printNumbers(arr, 6);
-// }
+void main(){
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    printNumbers(arr, 6);
+}
 
 // void printNumbers(int arr[],int n){
 //     for (int i = 0; i < n;i++){
