aa30c08033edcdc1446514bf72995b608436e191
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-09 17:08:11 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1626,3 +1626,15 @@ int countodd(int arr[], int n){
     }
     return count;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p = &a;
+
+    printf("Value of a = %d\n", *p);
+
+    return 0;
+}
