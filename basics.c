35b301954ba3563f3c8019a66d5c2e4fbf999022
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-09 17:09:01 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1651,3 +1651,22 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    swap(&x, &y);
+
+    printf("x = %d\ny = %d", x, y);
+
+    return 0;
+}
