7300c170e869309aae09d50dff32a1977314df86
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-09 17:09:24 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1670,3 +1670,17 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int *p = arr;
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", *(p + i));
+    }
+
+    return 0;
+}
