406a31283235b07e2fd87fdfd719c5007aad7f85
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-09 17:10:50 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1735,3 +1735,18 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p = &a;
+    int **q = &p;
+
+    printf("a = %d\n", a);
+    printf("*p = %d\n", *p);
+    printf("**q = %d\n", **q);
+
+    return 0;
+}
