2fa41bdfe5b2be6fac914a52ac45f59bf94b4216
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-10 16:56:31 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1935,3 +1935,29 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, j, temp;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < 5; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+
+    for(i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
