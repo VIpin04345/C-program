a3bcb7891bf6163957336a2d5c994bc6378a68f8
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-10 16:54:11 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1856,3 +1856,30 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, num, found = 0;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter number to search: ");
+    scanf("%d", &num);
+
+    for(i = 0; i < 5; i++) {
+        if(arr[i] == num) {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found)
+        printf("Number found at position %d", i + 1);
+    else
+        printf("Number not found");
+
+    return 0;
+}
