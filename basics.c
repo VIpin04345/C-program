9cfc76ee02a77a8f066e4f5810984452fd1db40a
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-10 16:52:55 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1791,3 +1791,24 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, min;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    min = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Smallest = %d", min);
+
+    return 0;
+}
