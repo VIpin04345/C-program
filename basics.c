436f8a19da8acd96cf5c79913e1849a3e61cd8de
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-10 16:57:43 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -650,6 +650,21 @@ int main(){
     
 }
 
+#include<stdio.h>
+int main(){
+    int n;
+    
+    printf("enter a number:-");
+    scanf("%d", &n);
+    int i = 0;
+    while (i<=n)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    
+}
+
 
 # write a program in c to display even numbers between five to fifteen.
 
