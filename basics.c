7dca76009e332882932acb0890fc3693a41475bb
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-10 16:52:28 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1770,3 +1770,24 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, max;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    max = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest = %d", max);
+
+    return 0;
+}
