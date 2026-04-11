ee0d88bcd52e8f6ce32f178269cf8b49a668f2b1
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-11 17:42:04 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2147,3 +2147,22 @@ int main() {
     printf("Vowels = %d\nConsonants = %d", vowels, consonants);
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    gets(str);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("Uppercase: %s", str);
+    return 0;
+}
