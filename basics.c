061f737f1b7cca5ff6bcd5d2829ddc093ba61240
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-11 17:39:29 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2017,3 +2017,22 @@ int main() {
     printf("Length = %d", i);
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i = 0;
+
+    printf("Enter string: ");
+    gets(str1);
+
+    while (str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+    str2[i] = '\0';
+
+    printf("Copied string: %s", str2);
+    return 0;
+}
