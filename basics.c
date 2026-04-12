103f549fac3ef1470a4ed7c73c7e052b1dec9c9a
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-12 12:52:30 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2132,6 +2132,26 @@ int main() {
 
 int main() {
     char str[100];
+    int i, len = 0;
+
+    gets(str);
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
     int i = 0, vowels = 0, consonants = 0;
 
     gets(str);
