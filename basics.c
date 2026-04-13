eabd083a79241fc078b05ec9f6c5401c0d523835
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-13 13:53:13 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -88,31 +88,31 @@ int main() {
     return 0;
 }
 // Array ko ascending order me sort karo.
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int arr[5], i, j, temp;
+int main() {
+    int arr[5], i, j, temp;
 
-//     for(i = 0; i < 5; i++)
-//         scanf("%d", &arr[i]);
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
 
-//     for(i = 0; i < 5; i++) {
-//         for(j = i + 1; j < 5; j++) {
-//             if(arr[i] > arr[j]) {
-//                 temp = arr[i];
-//                 arr[i] = arr[j];
-//                 arr[j] = temp;
-//             }
-//         }
-//     }
+    for(i = 0; i < 5; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 
-//     printf("Sorted array: ");
+    printf("Sorted array: ");
 
-//     for(i = 0; i < 5; i++)
-//         printf("%d ", arr[i]);
+    for(i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
 
-//     return 0;
-// }
+    return 0;
+}
 // Array me duplicate elements find karo.
 // #include <stdio.h>
 
