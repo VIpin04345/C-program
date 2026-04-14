9530047310b07db7bcb79420accad92d3a1caf0a
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-14 15:42:27 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2508,3 +2508,29 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int arr[5], i;
+    int largest, second;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    largest = second = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second Largest = %d", second);
+
+    return 0;
+}
