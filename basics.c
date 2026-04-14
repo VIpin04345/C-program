0ad7f6fbff05585f05a0f1c98fc0f47b03a63bf9
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-14 15:44:53 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -1899,6 +1899,30 @@ int main() {
 }
 
 
+
+#include <stdio.h>
+
+int main() {
+    int arr[10], i, even = 0, odd = 0;
+
+    for(i = 0; i < 10; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < 10; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d\n", even);
+    printf("Odd = %d", odd);
+
+    return 0;
+}
+
+
+
 #include <stdio.h>
 
 int main() {
