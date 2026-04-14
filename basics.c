8c0181f8f4f6659c3c7ec77951c92d17a3f66a3a
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-14 15:44:22 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2319,6 +2319,26 @@ int main() {
 }
 
 
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    gets(str);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("Uppercase: %s", str);
+    return 0;
+}
+
+
 
 Array me sabse chhota element find karo.
 #include <stdio.h>
