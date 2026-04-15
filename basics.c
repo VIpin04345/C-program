50f39bbd204194bf2d6dfe13a5ea64062dadcc7b
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-15 13:46:51 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -534,6 +534,14 @@ int main(){
 
 #include<stdio.h>
 int main(){
+    for (int i = 1; i <= 50;i++){
+        printf("%d \n", i);
+    }
+}
+
+
+#include<stdio.h>
+int main(){
     int i=1;
     while (i<=5)
     {
