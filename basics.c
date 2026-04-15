60dc14f8371a99b094f5bac84b998e017a79bb0f
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-15 13:49:59 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -693,6 +693,22 @@ int main(){
     
 }
 
+
+#include<stdio.h>
+int main(){
+    int n;
+    
+    printf("enter a number:-");
+    scanf("%d", &n);
+    int i = 0;
+    while (i<=n)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    
+}
+
 #include<stdio.h>
 int main(){
     int n;
