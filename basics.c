08c0e5229c27973d593f00bffa0966f7c040a7b3
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-16 10:38:57 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2913,24 +2913,24 @@ int main() {
 //     return 0;
 // }
 // 4. String Copy (Without strcpy)
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     char str1[100], str2[100];
-//     int i = 0;
+int main() {
+    char str1[100], str2[100];
+    int i = 0;
 
-//     printf("Enter string: ");
-//     gets(str1);
+    printf("Enter string: ");
+    gets(str1);
 
-//     while (str1[i] != '\0') {
-//         str2[i] = str1[i];
-//         i++;
-//     }
-//     str2[i] = '\0';
+    while (str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+    str2[i] = '\0';
 
-//     printf("Copied string = %s", str2);
-//     return 0;
-// }
+    printf("Copied string = %s", str2);
+    return 0;
+}
 // 5. String Compare (Without strcmp)
 #include <stdio.h>
 
