4a269069ef2b2baf04bdfebb74bd7569204a51c3
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-17 14:18:17 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -2911,6 +2911,33 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, flag = 0, len;
+
+    printf("Enter string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len/2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
 // 4. String Copy (Without strcpy)
 #include <stdio.h>
 
