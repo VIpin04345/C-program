924e56666dd5cf7fbc10ca9b5ce01ed542a331a3
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-19 11:25:36 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -3289,24 +3289,24 @@ int main() {
     return 0;
 }
 // 🔹 10. Array ka Maximum Element
-// #include <stdio.h>
-// int main() {
-//     int arr[5], i, max;
+#include <stdio.h>
+int main() {
+    int arr[5], i, max;
 
-//     printf("Enter 5 elements: ");
-//     for(i = 0; i < 5; i++) {
-//         scanf("%d", &arr[i]);
-//     }
+    printf("Enter 5 elements: ");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-//     max = arr[0];
+    max = arr[0];
 
-//     for(i = 1; i < 5; i++) {
-//         if(arr[i] > max)
-//             max = arr[i];
-//     }
+    for(i = 1; i < 5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
 
-//     printf("Max = %d", max);
-//     return 0;
-// }
+    printf("Max = %d", max);
+    return 0;
+}
 
 
