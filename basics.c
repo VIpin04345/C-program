75dc0e8e0a28987ccfcb01c2ebda847dac884597
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-19 11:28:09 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -3236,6 +3236,23 @@ int main() {
     printf("Reversed = %d", rev);
     return 0;
 }
+
+#include <stdio.h>
+int main() {
+    int n, rev = 0, rem;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    printf("Reversed = %d", rev);
+    return 0;
+}
 // 🔹 7. Palindrome Number
 #include <stdio.h>
 int main() {
