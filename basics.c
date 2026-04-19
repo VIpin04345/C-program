0ad5390871ea9c0ec93d9fa884bde7765cc07826
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-19 11:25:57 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -3310,3 +3310,25 @@ int main() {
 }
 
 
+
+#include <stdio.h>
+int main() {
+    int arr[5], i, max;
+
+    printf("Enter 5 elements: ");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    for(i = 1; i < 5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Max = %d", max);
+    return 0;
+}
+
+
