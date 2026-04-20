da6a13b4e2aea0144b4e11e649b4a34b81e2437a
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-20 12:59:18 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -52,7 +52,20 @@ int main() {
     return 0;
 }
 
+#include<stdio.h>
+int main() {
+    int n, rev = 0, rem;
+    scanf("%d", &n);
 
+    while(n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    printf("Reverse = %d", rev);
+    return 0;
+}
 
 
 
