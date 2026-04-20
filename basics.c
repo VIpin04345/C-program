31a1593fd126b0245e6f661f5f892055fce83939
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-20 12:59:50 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -85,7 +85,25 @@ int main() {
 
 
 
+#include<stdio.h>
+int main() {
+    int n, temp, rev = 0, rem;
+    scanf("%d", &n);
 
+    temp = n;
+    while(n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    if(temp == rev)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
 
 
 
