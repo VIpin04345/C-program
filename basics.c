1d3f078d2cb20c809928ec504f948d747a9acd07
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-20 12:58:22 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -11,7 +11,16 @@ int main() {
     return 0;
 }
 
-
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
 
 
 
