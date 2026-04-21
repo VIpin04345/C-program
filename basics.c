de656c4253d1c81827a2b414aba0abd7089a3933
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-21 10:13:05 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -44,10 +44,15 @@ int main() {
         printf("Odd");
     return 0;
 }
-
-
-
-
+#include<stdio.h>
+int main() {
+    int n, i, fact = 1;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+        fact *= i;
+    printf("Factorial = %d", fact);
+    return 0;
+}
 
 #include<stdio.h>
 int main() {
@@ -58,6 +63,13 @@ int main() {
     printf("Factorial = %d", fact);
     return 0;
 }
+
+
+
+
+
+
+
 
 #include<stdio.h>
 int main() {
